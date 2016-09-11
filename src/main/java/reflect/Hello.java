dee0a939f74ae0a87a081aa70f53a3c67a49e9ab
Debri <2290832242@qq.com>
2016-09-11 16:03:58 +0800

    Day 9.11_2

--- src/main/java/reflect/Hello.java
+++ src/main/java/reflect/Hello.java
@@ -2,8 +2,6 @@ package reflect;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Liuqi
@@ -12,16 +10,37 @@ import java.util.List;
  * Project: Demos
  */
 class Demo{
-    public  String name ;
+    private  String name ;
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
 
 public class Hello {
-    public static void main(String[] args) {
-        Demo demo=new Demo();
-        String cname = demo.getClass().getName() ;
-        Field [] fields = demo.getClass().getFields() ;
-        Method [] methods = demo.getClass().getDeclaredMethods();
+    public static void main(String[] args) throws Exception{
+       // Demo demo=new Demo();
+
+        Class<?> cls = Class.forName("reflect.Demo");
+        Object obj=cls.newInstance();
+        String cname = obj.getClass().getName() ;
+        Field [] fields = obj.getClass().getFields() ;
+        Method [] methods = obj.getClass().getDeclaredMethods();
+        Method setMed=cls.getClass().getDeclaredMethod("setName",String.class);
+        Method getMed=cls.getClass().getDeclaredMethod("getName");
+        //Method setMed=cls.getDeclaredMethod("setName",String.class);//这两行代码和上面两行代码功能一致
+       // Method getMed=cls.getDeclaredMethod("getName");
+        setMed.invoke(obj,"zhizhang");
+        System.out.println(getMed.invoke(obj));
+   //虽然可以这样设置值，但最好不要这样做，一定要通过gettter和setter方法来获取和改变值
+/*      Field field = obj.getClass().getField("name") ;
+        field.set(obj,"mingzi");
+        System.out.println(field.get(obj));*/
+
         System.out.println(cname);
         for (int i =0 ; i < fields.length ; i++){
             System.out.print(fields[i]+" ");
