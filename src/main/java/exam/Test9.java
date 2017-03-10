6606f45cf4ec5f0e2a77333175ecc07fb09b1669
Debri <18908356464@163.com>
2017-03-10 21:55:55 +0800

    3.8

---
+++ src/main/java/exam/Test9.java
@@ -0,0 +1,14 @@
+package exam;
+
+import java.io.File;
+
+/**
+ * Created by Liuqi
+ * Date: 2017/3/10.
+ */
+public class Test9 {
+    public static void main(String[] args) {
+        File file = new File("e:\\work\\108\\liyi\\508\\\\2014210925-0331402,2014210925 任秀梅.docx");
+        System.out.println(file.length());
+    }
+}
